8ab9f7fbed37a5e58ab99b3c633bd6443b0d9058
Igor Minar <igor@angularjs.org>
2016-06-28 15:01:50 +0000

    24eb838 fix: public api surface fixes + stability markers

--- bundles/router-deprecated.umd.js
+++ bundles/router-deprecated.umd.js
@@ -37,6 +37,9 @@ var __extends = (this && this.__extends) || function (d, b) {
     // Need to declare a new variable for global here since TypeScript
     // exports the original value of the symbol.
     var global$1 = globalScope;
+    /**
+     * @stable
+     */
     var Type$1 = Function;
     function getTypeNameForDebugging(type) {
         if (type['name']) {
